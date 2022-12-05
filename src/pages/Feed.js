2ca5f531b574e868d0a4490017647b2ebbf3e51d
Wenceslauu <joaovitor.campagninin@hotmail.com>
2022-12-05 12:19:18 -0300

    Add profile pic on menu

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,25 +1,11 @@
-import { useState } from 'react'
-
-import Post from '../components/Post'
-import PostChunk from '../components/PostChunk'
+import PostSection from '../components/PostSection'
 
 function Feed() {
-    const [newPosts, setNewPosts] = useState([])
-
-    const newPostItems = newPosts.map((post) => {
-        return (
-            <Post key={post._id} post={post} />
-        )
-    })
-
     return (
         <>
             <div className='flex gap-12 px-0 sm:px-8 md:px-40 items-start'>
                 <div className='basis-0 grow-[3] bg-base-300 rounded-box p-4 min-w-0'>
-                    <ul className='flex flex-col gap-4'>
-                        {newPostItems}
-                        <PostChunk page='0' />
-                    </ul>
+                    <PostSection />
                 </div>
                 <div className='basis-0 grow-[1] bg-base-300 rounded-box p-4 hidden lg:block'>
                     friend suggestions
--- src/pages/ProtHeaderLayout.js
+++ src/pages/ProtHeaderLayout.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, Outlet, useNavigate } from 'react-router-dom'
 
 import { ServerErrorContext } from '../contexts/ServerErrorContext'
@@ -6,14 +6,37 @@ import { ServerErrorContext } from '../contexts/ServerErrorContext'
 import ErrorBox from '../components/ErrorBox'
 import FriendRequests from '../components/FriendRequests'
 import Avatar from '../components/Avatar'
+import { loadOwnUser } from '../services/UserServices'
 
 function ProtHeaderLayout() {
+    const [user, setUser] = useState({})
     const [serverErrors, setServerErrors] = useState([])
     const [showServerErrors, setShowServerErrors] = useState(false)
 
     const value = { setServerErrors, setShowServerErrors }
 
     const navigate = useNavigate()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await loadOwnUser()
+
+                console.log(data.user)
+                setUser(data.user)
+            } catch (error) {
+                if (error.response.status === 401) {
+                    localStorage.removeItem('user')
+                    navigate('/login')
+                } else {
+                    setServerErrors([error.response.data.error])
+                    setShowServerErrors(true)
+                }
+            }
+        }
+
+        fetchData()
+    }, [navigate, setServerErrors, setShowServerErrors])
 
     function logout() {
         localStorage.removeItem('user')
@@ -35,9 +58,10 @@ function ProtHeaderLayout() {
                             {/* A <label tabindex="0"> is used here instead of a <button> because Safari has
                                 a bug that prevents the button from being focused. */}
                             <label tabIndex='0' className='btn btn-ghost btn-circle'>
-                                <Avatar alt="You" />
+                                <Avatar src={user.photo} alt="You" />
                             </label>
                             <ul tabIndex='0' className='menu menu-compact dropdown-content mt-4 p-2 shadow rounded-box w-52 bg-base-300'>
+                                <li className='font-bold py-2 px-4'>{user.username}</li>
                                 <li><Link to='profile'>Profile</Link></li>
                                 <li><button onClick={logout}>Logout</button></li>
                             </ul>
