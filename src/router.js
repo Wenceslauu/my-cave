116777fe78ede8be650f1d9c95271fe6d0a17e51
Wenceslauu <joaovitor.campagninin@hotmail.com>
2023-01-10 16:28:01 -0300

    Disable strict mode and change routes

--- src/router.js
+++ src/router.js
@@ -2,57 +2,63 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
-import FooterLayout from './pages/FooterLayout'
-import ProtHeaderLayout from './pages/ProtHeaderLayout'
-import UnprotHeaderLayout from './pages/UnprotHeaderLayout.js'
 import Feed from './pages/Feed'
 import Profile from './pages/Profile'
 import Users from './pages/Users'
-import Error from './pages/Error'
+import NotFound from './pages/NotFound'
 
+import FooterLayout from './components/FooterLayout'
+import UnprotHeaderLayout from './components/UnprotHeaderLayout.js'
+import ProtHeaderLayout from './components/ProtHeaderLayout'
+import ServerMessage from './components/ServerMessage'
 import Protected from './components/Protected'
 import Unprotected from './components/Unprotected'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <FooterLayout />,
-    errorElement: <Error />,
+    element: <ServerMessage />,
+    errorElement: <NotFound />,
     children: [
       {
-        element: <Unprotected><UnprotHeaderLayout /></Unprotected>,
+        element: <FooterLayout />,
         children: [
           {
-            index: true,
-            element: <Home />
+            element: <Unprotected><UnprotHeaderLayout /></Unprotected>,
+            children: [
+              {
+                index: true,
+                element: <Home />
+              },
+              {
+                path: "login",
+                element: <Login />
+              },
+              {
+                path: "signup",
+                element: <SignUp />
+              }
+            ]
           },
           {
-            path: "login",
-            element: <Login />
+            element: <Protected><ProtHeaderLayout /></Protected>,
+            children: [
+              {
+                path: "feed",
+                element: <Feed />
+              },
+              {
+                path: "users",
+                element: <Users />
+              },
+              {
+                path: "users/:userID",
+                element: <Profile />
+              }
+            ]
           },
-          {
-            path: "signup",
-            element: <SignUp />
-          }
         ]
-      },
-      {
-        element: <Protected><ProtHeaderLayout /></Protected>,
-        children: [
-          {
-            path: "feed",
-            element: <Feed />
-          },
-          {
-            path: "profile",
-            element: <Profile />
-          },
-          {
-            path: "users",
-            element: <Users />
-          }
-        ]
-      },
+      }
     ]
   }
 ])
